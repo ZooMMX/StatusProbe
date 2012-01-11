7a003c064c5c84927f72f4ef85ecbb766e13dfda
Octavio Ruiz <octavio@servicios-corporativos.com>
2012-01-11 13:15:28 -0600

    Log4j appender para AirBreak

--- src/com/phesus/statusq/Main.java
+++ src/com/phesus/statusq/Main.java
@@ -1,7 +1,11 @@
 package com.phesus.statusq;
 
+import code.lucamarrocco.hoptoad.HoptoadNotice;
+import code.lucamarrocco.hoptoad.HoptoadNoticeBuilder;
+import code.lucamarrocco.hoptoad.HoptoadNotifier;
 import com.phesus.statusq.DAL.ExtractorOmoikane;
 import com.phesus.statusq.ServiceLayer.PubnubWS;
+import org.apache.log4j.Logger;
 
 /**
  * Proyecto Omoikane: SmartPOS 2.0
@@ -10,11 +14,10 @@ import com.phesus.statusq.ServiceLayer.PubnubWS;
  * Time: 20:11
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         ExtractorOmoikane etl      = new ExtractorOmoikane();
         PubnubWS          ws       = new PubnubWS(etl);
         ws.iniciar(true);
-
     }
 }
